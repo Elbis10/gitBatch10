c2afe0678b34b3561ec86b915fd842358f253cc8
Elbis10 <elbism7@gmail.com>
2021-11-02 19:52:17 -0400

    basic framework is ready

---
+++ src/com/syntax/class1/TestingClass.java
@@ -0,0 +1,11 @@
+package com.syntax.class1;
+
+import org.testng.annotations.Test;
+
+public class TestingClass {
+
+    @Test
+    public void testMethod(){
+        System.out.println("this is my first method");
+    }
+}
